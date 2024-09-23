8d8f899cd5ebb0062bfb3106e4d8e7eb3b9dd7e3
legacy69cat <basscore357@gmail.com>
2024-09-23 21:39:31 +0500

    a

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-<?php
-echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php  
+   
+
+
+
+
+
+
+
+
+
+    ?>
+    <p>Madi gay</p>
+</body>
+</html>
