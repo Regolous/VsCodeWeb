a2f29045bf84a6b9fb63c6116994eea5f852b59d
Regolous <orazgaliyevmadi@gmail.com>
2024-09-30 20:50:48 +0600

    index change

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+$db_server = "localhost:3306";
+$db_user = "root";
+$db_pass = "";
+$db_name = "projectDB";
+
+$conn = "";
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
